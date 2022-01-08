f7f44e6e6e7b445ec89b07a8bd99832953693096
psmever <psmever@gmail.com>
2022-01-08 17:45:02 +0900

    [UPDATE] 결제.

--- app/Route/default.php
+++ app/Route/default.php
@@ -19,6 +19,7 @@ Route::add('/v1/order', function() {
     } else {
         $view = file_get_contents(VIEWS . '/' . 'order.html');
 
+        $view = str_replace('^order_client_key^', $_ENV['TOSS_CLIENT_KEY'], $view);
         $view = str_replace('^order_uuid^', $order['result']['uuid'], $view);
         $view = str_replace('^order_name^', $order['result']['order_name'], $view);
         $view = str_replace('^order_price^', $order['result']['order_price'], $view);
@@ -31,13 +32,66 @@ Route::add('/v1/order', function() {
 Route::add('/v1/success', function() {
     Databases::init();
 
-    echo "success";
+    $paymentKey = $_GET['paymentKey'];
+    $orderId = $_GET['orderId'];
+    $amount = $_GET['amount'];
+
+    $secretKey = $_ENV['TOSS_SECRET_KEY'];
+
+    $url = 'https://api.tosspayments.com/v1/payments/' . $paymentKey;
+
+    $data = ['orderId' => $orderId, 'amount' => $amount];
+
+    $credential = base64_encode($secretKey . ':');
+
+    $curlHandle = curl_init($url);
+
+    curl_setopt_array($curlHandle, [
+        CURLOPT_POST => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_HTTPHEADER => [
+            'Authorization: Basic ' . $credential,
+            'Content-Type: application/json'
+        ],
+        CURLOPT_POSTFIELDS => json_encode($data)
+    ]);
+
+    $response = curl_exec($curlHandle);
+
+    $httpCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+    $isSuccess = $httpCode == 200;
+    $responseJson = json_decode($response);
+
+    if ($isSuccess) {
+        $view = file_get_contents(VIEWS . '/' . 'success.html');
+
+        $view = str_replace('^front_url^', $_ENV['FRONT_URL'], $view);
+
+        echo $view;
+
+    } else {
+//        echo "<h1>결제 실패</h1>";
+//        echo "<p>" .$responseJson->message . "</p>";
+//        echo "<span>에러코드:" . $responseJson->code . "</span>";
+
+        $view = file_get_contents(VIEWS . '/' . 'fail.html');
+
+        $view = str_replace('^front_url^', $_ENV['FRONT_URL'], $view);
+        echo $view;
+    }
+
 }, 'get');
 
 Route::add('/v1/fail', function() {
     Databases::init();
 
-    echo "fail";
+
+    $view = file_get_contents(VIEWS . '/' . 'fail.html');
+
+    $view = str_replace('^front_url^', $_ENV['FRONT_URL'], $view);
+
+    echo $view;
+
 }, 'get');
 
 Route::run(BASEPATH);
