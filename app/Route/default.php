387fd37061a30981d9f361232eec2b9fd0250f58
psmever <psmever@gmail.com>
2022-01-08 16:53:12 +0900

    [UPDATE] 결제.

--- app/Route/default.php
+++ app/Route/default.php
@@ -1,12 +1,43 @@
 <?php
+
+use App\Traits\Databases;
 use App\Util\Route;
-use App\Controller\OrderController;
 
 Route::add('/test', function() {
     echo BASEPATH;
 }, 'get');
 
 
-Route::add('/v1/order', OrderController::order(), 'get');
+Route::add('/v1/order', function() {
+
+    Databases::init();
+
+    $order = Databases::getOrderData($_GET['uuid']);
+
+    if($order['state'] === false) {
+        echo "error";
+    } else {
+        $view = file_get_contents(VIEWS . '/' . 'order.html');
+
+        $view = str_replace('^order_uuid^', $order['result']['uuid'], $view);
+        $view = str_replace('^order_name^', $order['result']['order_name'], $view);
+        $view = str_replace('^order_price^', $order['result']['order_price'], $view);
+        $view = str_replace('^order_price2^', number_format($order['result']['order_price']), $view);
+
+        echo $view;
+    }
+}, 'get');
+
+Route::add('/v1/success', function() {
+    Databases::init();
+
+    echo "success";
+}, 'get');
+
+Route::add('/v1/fail', function() {
+    Databases::init();
+
+    echo "fail";
+}, 'get');
 
 Route::run(BASEPATH);
