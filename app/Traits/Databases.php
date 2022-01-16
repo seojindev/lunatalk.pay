c36025fd76401aceb148c6e9427a724923236c82
psmever <psmever@gmail.com>
2022-01-16 23:07:30 +0900

    [UPDATE] 결제 기록.

--- app/Traits/Databases.php
+++ app/Traits/Databases.php
@@ -61,7 +61,7 @@ trait Databases
 
         $db->where ('uuid', $uuid);
 
-        $db->update ('order_payments', [
+        $db->update ('order_masters', [
             'active' => 'Y',
         ]);
     }
